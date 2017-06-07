e260e7710404a431a6e0e5818daac6b0ca26ddcb
lecik <lec1k2103@gmail.com>
2017-06-07 11:01:37 +0300

    travisci support added(headless test)

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,7 +3,7 @@ Bundler.require(:test)
 Dir[File.dirname(__FILE__) + '/../../model/*.rb'].each { |file| require file }
 
 Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
+  Capybara::Selenium::Driver.new(app, browser: :chrome, switches: %w[--headless --disable-gpu ----remote-debugging-port=9222])
 end
 
 Capybara.configure do |config|
